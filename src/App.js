282de6b1729505bdc45e553d1454fd8e60f0df17
Sudha1919 <sudhabangaru1919@gmail.com>
2021-09-29 19:02:04 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./style.css";
-import Arrowfn from './Arrowfn'
+import Arrowfn from './Arrowfn';
+import Errorboundary from './Errorboundary';
+import Error from './Error'
 
 export default function App() {
   return (
     <div>
-      <Arrowfn />
+      <Error>
+     <Errorboundary heroName='batsman' />
+     <Errorboundary heroName='superman' />
+     <Errorboundary heroName='jokery' />
+     </Error>
     </div>
   );
 }
